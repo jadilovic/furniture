be0df4497251ef6ff4afa651876d323741590313
jadilovic <adilovic79@yahoo.com>
2020-07-18 23:38:54 +0200

    search page

---
+++ src/main/java/com/avlija/furniture/controller/SearchController.java
@@ -0,0 +1,76 @@
+package com.avlija.furniture.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.avlija.furniture.form.SampleInputs;
+import com.avlija.furniture.model.Element;
+import com.avlija.furniture.model.ElementQuantity;
+import com.avlija.furniture.model.Order;
+import com.avlija.furniture.model.Product;
+import com.avlija.furniture.model.ProductElement;
+import com.avlija.furniture.model.UnitMeasure;
+import com.avlija.furniture.repository.ElementQuantityRepository;
+import com.avlija.furniture.repository.ElementRepository;
+import com.avlija.furniture.repository.OrderRepository;
+import com.avlija.furniture.repository.ProductRepository;
+import com.avlija.furniture.repository.UnitMeasureRepository;
+import com.avlija.furniture.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SearchController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ElementRepository elementRepository;
+    
+    @Autowired
+    private ProductRepository productRepository;
+    
+    @Autowired
+    private ElementQuantityRepository elementQuantityRepository;
+    
+    // SEARCH PRODUCTS
+    
+    @RequestMapping(value= {"home/productsearch"}, method=RequestMethod.GET)
+    public ModelAndView allElements() {
+     ModelAndView model = new ModelAndView();
+     SampleInputs sampleInputs = new SampleInputs();
+     model.addObject("sampleInputs", sampleInputs);
+     model.setViewName("home/search_product");
+     
+     return model;
+    }
+    
+    
+    private List<ElementQuantity> getElementQuantityList(List<Element> elementList, Product product) {
+   	 List<ElementQuantity> elementQuantitiyList = new ArrayList<ElementQuantity>();
+   	 for(Element element: elementList) {
+   		 ElementQuantity elementQuantity;
+   		 try {
+   			 elementQuantity = elementQuantityRepository.findById(new ProductElement(product.getId(), element.getId())).get();
+   		 } catch(Exception e) {
+   			 elementQuantity = new ElementQuantity(new ProductElement(product.getId(), element.getId()), 0);
+   			 // productQuantityRepository.save(productQuantity);
+   		 }
+   		 elementQuantitiyList.add(elementQuantity);
+   	 }
+   	return elementQuantitiyList;
+   }
+}
