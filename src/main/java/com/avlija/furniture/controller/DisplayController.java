903b9642ed8014786b13b9c00182797c1ecf705b
jadilovic <adilovic79@yahoo.com>
2021-01-10 10:00:32 +0100

    crate word file in the app

--- src/main/java/com/avlija/furniture/controller/DisplayController.java
+++ src/main/java/com/avlija/furniture/controller/DisplayController.java
@@ -33,10 +33,18 @@ import com.avlija.furniture.service.ProductIdComp;
 import com.avlija.furniture.service.UserService;
 import com.avlija.furniture.form.LocalDateAttributeConverter;
 
+import org.docx4j.jaxb.Context;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.BooleanDefaultTrue;
+import org.docx4j.wml.Color;
+import org.docx4j.wml.ObjectFactory;
+import org.docx4j.wml.P;
+import org.docx4j.wml.R;
+import org.docx4j.wml.RPr;
+import org.docx4j.wml.Text;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -182,7 +190,26 @@ public class DisplayController {
 		}
         MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
         mainDocumentPart.addStyledParagraphOfText("Title", "Hello World!");
-        mainDocumentPart.addParagraphOfText("Welcome To Baeldung");
+        mainDocumentPart.addParagraphOfText("Welcome To Baeldung!");
+        
+        ObjectFactory factory = Context.getWmlObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
+        Text t = factory.createText();
+        t.setValue("Green Welcome To Baeldung");
+        r.getContent().add(t);
+        p.getContent().add(r);
+        RPr rpr = factory.createRPr();       
+        BooleanDefaultTrue b = new BooleanDefaultTrue();
+        rpr.setB(b);
+        rpr.setI(b);
+        rpr.setCaps(b);
+        Color green = factory.createColor();
+        green.setVal("green");
+        rpr.setColor(green);
+        r.setRPr(rpr);
+        mainDocumentPart.getContent().add(p);
+        
         File exportFile = new File("welcome.docx");
         try {
 			wordPackage.save(exportFile);
